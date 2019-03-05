24834e49b27af99bfa1c7c34773f2cfc32a6a23a
kfang9357 <43207499+kfang9357@users.noreply.github.com>
2019-03-04 21:53:19 -0500

    fixed perfect shuffle

--- src/com/company/Shuffler.java
+++ src/com/company/Shuffler.java
@@ -6,7 +6,7 @@ public class Shuffler {
     public static void main(String[] args) {
         System.out.println("Results of " + SHUFFLE_COUNT +
                 " consecutive perfect shuffles:");
-        int[] values1 = {0, 1, 2, 3};
+        int[] values1 = {1,2,3,4,5,6,7,8,9,10};
         for (int j = 1; j <= SHUFFLE_COUNT; j++) {
             perfectShuffle(values1);
             System.out.print("  " + j + ":");
@@ -33,21 +33,23 @@ public class Shuffler {
 
     public static void perfectShuffle(int[] values) {
         int[] shuffled = new int[values.length];
+        int mid = (values.length + 1)/2;
 
         int k = 0;
-        for (int j = 0; j < 25; j++) {
+        for (int j = 0; j < mid; j++) {
             shuffled[k] = values[j];
             k += 2;
         }
 
         k = 1;
-        for (int j = 26; j < values.length; j++) {
+        for (int j = mid; j < values.length; j++) {
             shuffled[k] = values[j];
             k += 2;
         }
 
-        for (int i = 0; i < values.length; i++) {
-            values[k] = shuffled[k];
+        for (int j = 0; j< values.length; j++)
+        {
+            values[j] = shuffled[j];
         }
     }
 
@@ -76,4 +78,26 @@ public class Shuffler {
 
  The selection shuffle algorithm does not require to a loop to find the largest (or smallest) value to swap, so
  it works quickly.
+ **/
+
+/** Consider the following “perfect shuffle” algorithm that starts with an array named cards that contains
+ 52 cards and creates an array named shuffled.
+ Initialize shuffled to contain 52 “empty” elements.
+ Set k to 0.
+ For j = 0 to 25,
+ − Copy cards[j] to shuffled[k];
+ − Set k to k+2.
+ Set k to 1.
+ For j = 26 to 51,
+ − Copy cards[j] to shuffled[k];
+ − Set k to k+2.
+
+ This approach moves the first half of cards to the even index positions of shuffled, and it moves
+ the second half of cards to the odd index positions of shuffled.
+ The above algorithm shuffles 52 cards. If an odd number of cards is shuffled, the array shuffled has
+ one more even-indexed position than odd-indexed positions. Therefore, the first loop must copy one
+ more card than the second loop does. This requires rounding up when calculating the index of the middle
+ of the deck. In other words, in the first loop j must go up to (cards.length + 1) / 2,
+ exclusive, and in the second loop j most begin at (cards.length + 1) / 2.
+
  **/
